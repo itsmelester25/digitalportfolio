5b58a9ee0dda4a3455c2cca87d22193dffe4495c
itsmelester25 <lhestherc@gmail.com>
2024-05-14 06:52:42 +0800

    Lester Chico

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,25 +1,54 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/portfolio-details',
-                '/assets/css',
-                '/assets/font',
-                '/assets/img',
-                '/assets/js',
-                '/assets/vendor',
-                '/assets/img/icon-app.png'
-            ]);
-        })
-    );
+const cache_name = 'lesters-portfolio-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/animate.css',
+  '/assets/css/bootstrap.css',
+  '/assets/css/bootstrap.css.map',
+  '/assets/css/flexslider.css',
+  '/assets/css/icomoon.css',
+  '/assets/css/main.css',
+  '/assets/css/style.css',
+  '/assets/css/style.css.map',
+  '/assets/fonts',
+  '/assets/img',
+  '/assets/js'
+
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cache_name)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [cache_name];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
